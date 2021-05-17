e225021b3ecd860d9f001cbe0b202b9459e22591
J Ben Anderson <revovisions@gmail.com>
2021-05-17 17:07:14 -0400

    changed onClick randomColor to initialize when first clicked

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
-const Square = ({ id }) => {
+const Square = ({ id, player }) => {
   const [color, setColor] = React.useState("green");
   const palet = ["red", "blue", "green", "yellow", "orange", "purple", "white"];
   const getRandomColor = () => palet[Math.floor(Math.random() * 7)];
+  React.useEffect(() => {
+    console.log(`Render ${id}`);
+    return () => console.log(`unmounting Square ${id}`);
+  });
+
   return (
     <button
       onClick={(e) => {
-        setColor(getRandomColor());
-        e.target.style.background = color;
+        let col = getRandomColor();
+        setColor(col);
+        e.target.style.background = col;
+        // alert(`I'm Square ${id}`); // testing onClick Player 1 or 2
       }}
     >
       <h1>{id}</h1>
@@ -16,9 +23,13 @@ const Square = ({ id }) => {
 
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
+  const [mounted, setMounted] = React.useState(true);
+  const [random, setRandom] = React.useState(0);
   let status = `Player ${player}`;
+  const toggle = () => setMounted(!mounted);
+  const reRender = () => setRandom(Math.random());
   function renderSquare(i) {
-    return <Square id={i}></Square>;
+    return <Square id={i} player={player}></Square>;
   }
   return (
     <div
@@ -29,11 +40,13 @@ const Board = () => {
       }}
     >
       <div className="grid-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+        {mounted && renderSquare(0)}
+        {mounted && renderSquare(1)}
+        {mounted && renderSquare(2)}
       </div>
       <div id="info">
+        <button onClick={toggle}>Show/Hide Row</button>
+        <button onClick={reRender}>Re-render</button>
         <h1>{status}</h1>
       </div>
     </div>
